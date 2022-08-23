54a62a40d1d2f49197c9afcc210cab75778569de
Spyridon Peppas <spyridonpeppas@iPhone8Sip7.station>
2022-08-23 18:48:27 +0300

    added points for teams

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ const placePointsPair = {
   "15": 1,
 }
 
+// const delay = async (millis) => {await new Promise((resolve, reject) => {setTimeout(_ => resolve(), millis)});}
+
 const fetchDrivers = async () => {
   try {
     const response = await axios.get("https://va8ozm5wii.execute-api.ap-northeast-1.amazonaws.com/api/v1/riders");
@@ -48,19 +50,20 @@ const fetchManufacturers = async () => {
 
 const upgradeDriversList = async (drivers) => {
   return await drivers.map(driver => {
-  let totalPoints = (placePointsPair[driver.prevRace1Position] || 0) + (placePointsPair[driver.prevRace2Position] || 0) + (placePointsPair[driver.prevRace3Position] || 0);
-  driver["totalPointsFromPrevEvents"] = totalPoints;
-  driver["pointEfficiencyRate"] = totalPoints / driver.cost;
-  return driver
-})}
+    let totalPoints = (placePointsPair[driver.prevRace1Position] || 0) + (placePointsPair[driver.prevRace2Position] || 0) + (placePointsPair[driver.prevRace3Position] || 0);
+    driver["totalPointsFromPrevEvents"] = totalPoints;
+    driver["pointEfficiencyRate"] = totalPoints / driver.cost;
+    return driver
+  })
+}
 
 const onSortDriversByEfficiency = async (drivers) => {
   return await drivers.sort((driverA, driverB) => {
-      if (driverA.pointEfficiencyRate === 0 && driverB.pointEfficiencyRate === 0) {
-        return (driverB.previousEvent - driverA.previousEvent)
-      }
-      return (driverB.pointEfficiencyRate - driverA.pointEfficiencyRate)
-    })
+    if (driverA.pointEfficiencyRate === 0 && driverB.pointEfficiencyRate === 0) {
+      return (driverB.previousEvent - driverA.previousEvent)
+    }
+    return (driverB.pointEfficiencyRate - driverA.pointEfficiencyRate)
+  })
 }
 
 const onSortDriversByPoints = async (drivers) => {
@@ -73,7 +76,6 @@ let myTeamTotalCost = 0;
 // let showNotification = false;
 
 function App() {
-
   const [drivers, setDrivers] = useState([]);
   const [myTeam, setMyTeam] = useState([]);
   const [manufacturers, setManufacturers] = useState([]);
@@ -89,18 +91,44 @@ function App() {
 
   const getManufacturers = async () => {
     let manufacturersArray = await fetchManufacturers();
+    manufacturersArray = await addPointsToManufacturers(manufacturersArray);
     setManufacturers(manufacturersArray);
+  }
+
+  const addPointsToManufacturers = async (manufacturersArray) => {
+    manufacturersArray = await [...manufacturersArray].map( team => {
+      let teamsDay1 = [];
+      let teamsDay2 = [];
+      let teamsDay3 = [];
+      // console.log(team.name);
+      for (let driver of drivers){
+        if (driver.manufacturerName === team.name){
+          teamsDay1.push(placePointsPair[driver?.prevRace1Position] || 0);
+          teamsDay2.push(placePointsPair[driver?.prevRace2Position] || 0);
+          teamsDay3.push(placePointsPair[driver?.prevRace3Position] || 0);
+        }
+      }
+      teamsDay1 = teamsDay1.sort((a,b)=>b-a);
+      teamsDay1 = [teamsDay1[0], teamsDay1[1]];
+      team["pointsDay1"] = teamsDay1;
+      teamsDay2 = teamsDay2.sort((a,b)=>b-a);
+      teamsDay2 = [teamsDay2[0], teamsDay2[1]];
+      team["pointsDay2"] = teamsDay2;
+      teamsDay3 = teamsDay3.sort((a,b)=>b-a);
+      teamsDay3 = [teamsDay3[0], teamsDay3[1]];
+      team["pointsDay3"] = teamsDay3;
+      return team
+    })
+    return manufacturersArray;
   }
 
   const sortDriversByPoints = async () => {
     let newDrivers = await onSortDriversByPoints(drivers);
-    console.log(newDrivers[0]);
     setDrivers([...newDrivers]);
   }
 
   const sortDriversByEfficiency = async () => {
     let newDrivers = await onSortDriversByEfficiency(drivers);
-    console.log(newDrivers[0]);
     setDrivers([...newDrivers]);
   }
 
@@ -134,15 +162,17 @@ function App() {
     }
   }
 
-  const showDriver = () => console.log(drivers[0]);
-
   useEffect(() => {
     getDrivers();
-    getManufacturers();
+    // getManufacturers();
   }, []);
 
+  useEffect(() => {
+    if (drivers.length === 0) return
+    getManufacturers();
+  }, [drivers]);
+
   const renderDriverList = drivers.map(driver => {
-    // console.log("driver");
     return <tr key={driver.id} className={`driver ${myTeam.includes(driver) ? 'selected' : ''}`} onClick={() => { toggleItemToMyTeam(driver); }}>
       <td> {driver?.name} </td>
       <td> {driver?.manufacturerName} </td>
@@ -206,6 +236,11 @@ function App() {
           <span>NAME</span>
           <span>COST</span>
           <span>LAST POS</span>
+
+          <span>Day1</span>
+          <span>Day2</span>
+          <span>Day3</span>
+          <span>Tot.Pnts</span>
         </div>
         <ul>
           {
@@ -214,6 +249,25 @@ function App() {
                 <span>{manufacturer.name}</span>
                 <span>{manufacturer.cost} $</span>
                 <span>{manufacturer.previousEvent}</span>
+                <div>
+                  <span style={{fontSize: '0.8rem'}}>{`(${manufacturer.pointsDay1[0]}&${manufacturer.pointsDay1[1]})`}</span>
+                  <span style={{ fontWeight: 'bold' }}>{ Math.ceil( (manufacturer.pointsDay1[0]+manufacturer.pointsDay1[1])/2 ) }</span>
+                </div>
+                <div>
+                  <span style={{fontSize: '0.8rem'}}>{`(${manufacturer.pointsDay2[0]}&${manufacturer.pointsDay2[1]})`}</span>
+                  <span style={{ fontWeight: 'bold' }}>{ Math.ceil( (manufacturer.pointsDay2[0]+manufacturer.pointsDay2[1])/2 ) }</span>
+                </div>
+                <div>
+                  <span style={{fontSize: '0.8rem'}}>{`(${manufacturer.pointsDay3[0]}&${manufacturer.pointsDay3[1]})`}</span>
+                  <span style={{ fontWeight: 'bold' }}>{ Math.ceil( (manufacturer.pointsDay3[0]+manufacturer.pointsDay3[1])/2 ) }</span>
+                </div>
+                <span style={{ fontWeight: 'bold' }}>
+                  {
+                    Math.ceil( (manufacturer.pointsDay1[0]+manufacturer.pointsDay1[1])/2 ) 
+                    + Math.ceil( (manufacturer.pointsDay2[0]+manufacturer.pointsDay2[1])/2 )
+                    + Math.ceil( (manufacturer.pointsDay3[0]+manufacturer.pointsDay3[1])/2 )
+                  }
+                </span>
               </li>
             ))
           }
